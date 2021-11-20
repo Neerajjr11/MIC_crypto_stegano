d53d9a272a3a06fc7b2c66f2d1a4dccae7eea2cd
Neeraj_jm <neerajjm12345@gmail.com>
2021-11-20 09:39:52 +0530

    Final_cryptography

---
+++ create_green.py
@@ -0,0 +1,15 @@
+from PIL import Image, ImageDraw, ImageFont
+from numpy import asarray
+import numpy as np
+#print(data)
+a = []
+for i in range(500):
+  b = []
+  for j in range(500):
+    b.append((255,255,255))
+  a.append(b)
+img = Image.fromarray(np.array(a).astype(np.uint8))
+myFont = ImageFont.truetype('FreeMono.ttf', 80)
+d1 = ImageDraw.Draw(img)
+d1.text((200, 200), "BOAT", font=myFont, fill =(0, 255, 0))
+img.save("image_text_green.png")
---
+++ encode_green.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageDraw, ImageFont
+from numpy import asarray
+import numpy as np
+image = Image.open('image_text_green.png')
+data = asarray(image)
+img = Image.open('stegano.png')
+dat = asarray(img)
+#print(data)
+ndata = []
+for i in range(500):
+  nrow = []
+  for j in range(500):
+    a,b,c = tuple(dat[i][j])
+    if tuple(data[i][j])==(0,255,0):
+      nrow.append((a+1,b,c))
+    else :
+      nrow.append((a,b,c))
+  ndata.append(nrow)
+img2 = Image.fromarray(np.array(ndata).astype(np.uint8))
+img2.save("encoded_green.png")
